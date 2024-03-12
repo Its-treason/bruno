c1a04f8febe115fbf018e4d2790779ec2ac63035
Its-treason <timon.sch71@gmail.com>
2024-03-12 17:43:38 +0100

    wip: typescript

---
+++ packages/bruno-common/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['cjs', 'es'],
+      fileName: 'index'
+    }
+  },
+  clearScreen: false
+});
---
+++ packages/bruno-graphql-docs/vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['cjs', 'es'],
+      fileName: 'index'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  },
+  clearScreen: false
+});
