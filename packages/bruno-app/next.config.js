c1b271e030a5c3c1929f4cbc797fb9137be09ddb
Its-Treason <timon.sch71@gmail.com>
2024-07-07 20:59:50 +0200

    feat: Remove webpack config

--- packages/bruno-app/next.config.js
+++ packages/bruno-app/next.config.js
@@ -3,12 +3,5 @@ module.exports = {
     CI: process.env.CI,
     PLAYWRIGHT: process.env.PLAYWRIGHT,
     ENV: process.env.ENV
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
   }
 };
