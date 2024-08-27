52688c258862ff3f12eb20e9b282b570c1d697d9
Its-Treason <timon.sch71@gmail.com>
2024-08-27 23:30:45 +0200

    refactor: Remove unused files

--- packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/RunCollectionItem/StyledWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  .bruno-modal-content {
-    padding-bottom: 1rem;
-  }
-`;
-
-export default Wrapper;
--- packages/bruno-app/src/components/Sidebar/Collections/CreateOrOpenCollection/StyledWrapper.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  color: ${(props) => props.theme.colors.text.muted};
-`;
-
-export default Wrapper;
