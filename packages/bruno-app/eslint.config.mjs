6d2353126350af27ce5c13d1615755f9b7e112dd
Its-Treason <timon.sch71@gmail.com>
2024-09-04 23:01:32 +0200

    chore: Update dependencies

--- packages/bruno-app/eslint.config.mjs
+++ packages/bruno-app/eslint.config.mjs
@@ -5,6 +5,7 @@ import reactJsx from 'eslint-plugin-react/configs/jsx-runtime.js';
 import react from 'eslint-plugin-react/configs/recommended.js';
 import globals from 'globals';
 import ts from 'typescript-eslint';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default [
   { languageOptions: { globals: globals.browser } },
