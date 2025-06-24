94ce6ddc37c8f8efc28f058359e15e6174d09126
Its-Treason <39559178+Its-treason@users.noreply.github.com>
2025-06-24 23:25:19 +0200

    fix

--- packages/bruno-electron/electron-builder-config.js
+++ packages/bruno-electron/electron-builder-config.js
@@ -1,5 +1,9 @@
 require('dotenv').config({ path: process.env.DOTENV_PATH });
 
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
 const config = {
   appId: 'com.usebruno-lazer.app',
   productName: 'Bruno lazer',
@@ -32,7 +36,9 @@ const config = {
     artifactName: 'bruno-lazer_nightly_${arch}_linux.${ext}',
     icon: 'resources/icons/png',
     executableName: 'bruno-lazer',
-    target: ['AppImage', 'deb', 'snap', 'rpm'],
+    target: ['AppImage', 'deb', 'snap', 'rpm']
+  },
+  snap: {
     compression: 'lzo'
   },
   win: {
